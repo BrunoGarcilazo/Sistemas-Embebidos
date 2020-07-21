e1b0e109104b8cb7adc312a5b5da0df305b04d12
FelipeMestre <feli13mestre@gmail.com>
2020-07-20 23:23:14 -0300

    Falta conseguir una funcion para cambiar de float a string en el GPS y queda

---
+++ ProyectoFinal.X/System/utils.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/** Descriptive File Name
+
+  @Company
+    Company Name
+
+  @File Name
+    filename.h
+
+  @Summary
+    Brief description of the file.
+
+  @Description
+    Describe the purpose of this file.
+ */
+/* ************************************************************************** */
+
+#ifndef _UTILS_H    /* Guard against multiple inclusion */
+#define _UTILS_H
+
+void ftoa(float n, char* res, int afterpoint);
+
+#endif
+
+/* *****************************************************************************
+ End of File
+ */
